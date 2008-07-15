7fccaf6bc2383604e8086d16f0715a83c921ee23
Robert Gogolok <gogolok@googlemail.com>
2008-07-16 01:05:13 +0200

    add AppController

---
+++ AppController.h
@@ -0,0 +1,16 @@
+//
+//  AppController.h
+//  GogoLock
+//
+//  Created by gogo on 7/16/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface AppController : NSObject {
+
+}
+
+@end
---
+++ AppController.m
@@ -0,0 +1,14 @@
+//
+//  AppController.m
+//  GogoLock
+//
+//  Created by gogo on 7/16/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppController.h"
+
+
+@implementation AppController
+
+@end
